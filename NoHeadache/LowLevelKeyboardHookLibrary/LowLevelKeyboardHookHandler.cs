538933e4fe143a9736984712146a7a9d6cf8cd35
DESKTOP-QERLRBA\juoza <juozaszilys@hotmail.com>
2024-07-30 20:03:29 +0300

    Fixed global usings.

--- NoHeadache/LowLevelKeyboardHookLibrary/LowLevelKeyboardHookHandler.cs
+++ NoHeadache/LowLevelKeyboardHookLibrary/LowLevelKeyboardHookHandler.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.InteropServices;
-
-namespace LowLevelKeyboardHookLibrary;
+﻿namespace LowLevelKeyboardHookLibrary;
 
 public static partial class LowLevelKeyboardHookHandler
 {
