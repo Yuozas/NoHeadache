679d974faf71dedc3ae1ab8fa3f7ca1b649070fc
DESKTOP-QERLRBA\juoza <juozaszilys@hotmail.com>
2024-07-30 20:49:42 +0300

    Switch pattern simplified.

--- NoHeadache/LowLevelKeyboardHookLibrary/ShiftHandler.cs
+++ NoHeadache/LowLevelKeyboardHookLibrary/ShiftHandler.cs
@@ -4,9 +4,8 @@ static class ShiftHandler
 {
 	public static bool TryShift(Keys key) => key switch
 	{
-		Keys.D0 or Keys.D1 or Keys.D2 or Keys.D3 or Keys.D4 or Keys.D5 or Keys.D6 or Keys.D7 or Keys.D8 or Keys.D9
-				=> HandleShiftKey(key),
-		_ => false
+		>= Keys.D0 and <= Keys.D9 => HandleShiftKey(key),
+		_                         => false
 	};
 	
 	private static bool HandleShiftKey(Keys key)
